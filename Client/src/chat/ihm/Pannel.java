4a22d8bae49464258d3e91c9a0c51be89c3e57cb
yassine <yassine@yassine-PC>
2013-04-24 16:31:13 +0200

    Module chat done

---
+++ Client/src/chat/ihm/Pannel.java
@@ -0,0 +1,41 @@
+package chat.ihm;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+@SuppressWarnings("serial")
+public class Pannel extends JPanel {
+	private JTextArea   _fieldChat;
+	// Je fais le choix de ne pas mettre de widget affichant la liste des participants pour le moment.
+	// Néanmoins je l'ai préparé :
+	// private JTable   _participants;
+	
+	private class South extends JPanel {
+		private JButton 	 _send;
+		private JTextField   _fieldForm;
+		
+		public South (){
+			_send = new JButton("Send message");
+			_fieldForm = new JTextField(32);
+			setLayout(new FlowLayout());
+			add(_fieldForm); add(_send);
+		}
+	}
+	
+	public Pannel () {
+		// _participants = new JTable(lemodele);
+		_fieldChat = new JTextArea(10,32);
+		_fieldChat.setEditable(false);
+		setLayout(new BorderLayout());
+        add(new JScrollPane(_fieldChat), BorderLayout.CENTER);
+        add(new South(), BorderLayout.SOUTH);
+		// est placé à droite (EAST) la liste des participants
+	}
+}
